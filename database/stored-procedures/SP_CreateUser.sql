65e765bc9048a40e2c291ea91312f759bbb1f51a
Xfighter32 <austinhakanson2@gmail.com>
2020-12-09 13:55:59 -0600

    Correciones a sp de creacion de usuario y se cambia el sp de obtener contraseña a una función que devuelve una tabla con el id y la contraseña dado un correo o username

--- database/stored-procedures/SP_CreateUser.sql
+++ database/stored-procedures/SP_CreateUser.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE PROCEDURE SP_CreateUser 
 (
-	pUserType VARCHAR,
+	pUserTypeId INT,
 	pName VARCHAR(100),
 	pLastName VARCHAR(100),
 	pBirthDate DATE,
@@ -16,11 +16,11 @@ BEGIN
     -- Check if user exists already
     IF NOT EXISTS(SELECT 1 FROM UserData UD WHERE UD.UserName = pUserName OR UD.Email = pEmail) 
     THEN 
-    	SELECT Id INTO TypeId FROM UserType UT WHERE UT.Type = pUserType;
+    	SELECT Id INTO TypeId FROM UserType UT WHERE UT.Id = pUserTypeId;
 		IF NOT FOUND 
 		THEN
 		    --RAISE EXCEPTION 'TypeId % not found', pUserType;
-		    status := -1; -- status -1 means failure.
+		    status := -1; -- The usertype doesn't exist.
 		ELSE
 			INSERT INTO UserData (UserTypeId, Name, LastName, BirthDate, UserName, Email, Password)
 	    	VALUES
@@ -34,12 +34,12 @@ BEGIN
 	    		pPassword
 	    	);
 	    	COMMIT;
-	    	status := 1; -- status 1 means success.
+	    	status := 1; -- User created succesfully.
 		END IF;   	
     ELSE
-    	--RAISE EXCEPTION 'El usuario ingresado ya existe en la base de datos';
+    	-- RAISE EXCEPTION 'El usuario ingresado ya existe en la base de datos';
     	ROLLBACK;
-    	status := -1; -- status -1 means failure.
+    	status := -2; -- The user already exists in the database.
     END IF;
 END; $BODY$
 LANGUAGE plpgsql;
